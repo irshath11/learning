ec1ae6beca9975163fc7436c003b451ec1eff4fa
Irshath Ahamed <irshath.ahamed@golden-hippo.com>
2023-08-11 12:07:04 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:adv_basics/ui.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals')
+        ),
+        body: const UIUpdatesDemo(),
+      ),
+    );
+  }
+}
